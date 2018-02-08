3b938b62777c50d9df40e34c7e442416f308fdb1
BobbyRockers <rrockers@gmail.com>
2018-02-07 19:09:04 -0600

    Added a count of foos and bars

--- foobar.cpp
+++ foobar.cpp
@@ -2,12 +2,21 @@
 using namespace std;
 
 int main(){
+	int foo = 0;
+	int bar = 0;
 	for(int i=1;i<=2000;i++){
 		cout << i;
-		if(i%50==0){ cout << " FOO"; }
-		if(i%33==0){ cout << " BAR"; }
+		if(i%50==0){ 
+			cout << " FOO"; 
+			foo++;
+		}
+		if(i%33==0){ 
+			cout << " BAR"; 
+			bar++;
+		}
 		cout << endl;
 	}
+	cout << "There were " << foo << " FOOs and " << bar << " BARs" << endl;
 	char done;
 	cin >> done;
 }
